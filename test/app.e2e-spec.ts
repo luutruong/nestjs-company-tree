698e5d40ae957ec8be513ab74ec12026daa9d077
Truong Luu <truonglv@outlook.com>
2024-01-05 18:41:35 +0700

    Add tests
    run with `yarn test:e2e`

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,92 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  const graphqlEndpoint = '/graphql';
+  let companyId: string | null = null;
+
+  describe(graphqlEndpoint, () => {
+    describe('company', () => {
+      it('createCompany', () => {
+        return request(app.getHttpServer())
+          .post(graphqlEndpoint)
+          .send({
+            query: `mutation {
+            createCompany(createCompanyInput: {
+              name: "test company",
+              parentId: null
+            }) {
+              id
+            }
+          }`,
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(typeof res.body.data.createCompany.id).toBe('string');
+            companyId = res.body.data.createCompany.id;
+          });
+      });
+
+      it('createCompanyTravel', () => {
+        return request(app.getHttpServer())
+          .post(graphqlEndpoint)
+          .send({
+            query: `mutation {
+              createCompanyTravel(createCompanyTravelInput: {
+                companyId: "${companyId}",
+                departure: "IT Test",
+                destination: "Hanoi",
+                employeeName: "Tester",
+                price: 10000
+              }) {
+                id
+              }
+            }`,
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(typeof res.body.data.createCompanyTravel.id).toBe('string');
+          });
+      });
+
+      it('company', () => {
+        return request(app.getHttpServer())
+          .post(graphqlEndpoint)
+          .send({
+            query: `{
+            company(id: "${companyId}") {
+              id
+              cost
+            }
+          }`,
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.company.id).toBe(companyId);
+            expect(res.body.data.company.cost).toBe(10000);
+          });
+      });
+
+      it('companyTree', () => {
+        return request(app.getHttpServer())
+          .post(graphqlEndpoint)
+          .send({
+            query: '{ companyTree { id }}',
+          })
+          .expect(200);
+      });
+
+      it('removeCompany', () => {
+        return request(app.getHttpServer())
+          .post(graphqlEndpoint)
+          .send({
+            query: `mutation { removeCompany(id: "${companyId}") }`,
+          })
+          .expect(200);
+      });
+    });
   });
 });
